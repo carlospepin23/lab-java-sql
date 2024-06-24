0a6a936eec95a78362a4af7f754fcd0ba5d874e6
carlospepin23 <carlos.pepin1@upr.edu>
2024-06-24 17:05:58 -0400

    normalized databases

--- lab-script.sql
+++ lab-script.sql
@@ -4,69 +4,80 @@ DROP SCHEMA IF EXISTS lab;
 CREATE SCHEMA lab;
 USE lab;
 
+CREATE TABLE authors (
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+	author_name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE blog (
-	author VARCHAR(255),
-	title VARCHAR(255),
-	word_count INT,
-	views INT
+	author_id INT,
+    title VARCHAR(255) NOT NULL,
+    word_count INT,
+    views INT,
+    FOREIGN KEY (author_id) REFERENCES authors(Id)
+);
+
+INSERT INTO authors (author_name) values
+	('Maria Charlotte'),
+	('Juan Perez'),
+	('Gemma Alcocer');
+
+INSERT INTO blog (author_id, title, word_count, views) VALUES
+    (1, 'Best Paint Colors', 814, 14),
+    (2, 'Small Space Decorating Tips', 1146, 221),
+    (1, 'Hot Accessories', 986, 105),
+    (1, 'Mixing Textures', 765, 22),
+    (2, 'Kitchen Refresh', 1242, 307),
+    (1, 'Homemade Art Hacks', 1002, 193),
+    (3, 'Refinishing Wood Floors', 1571, 7542);
+
+CREATE TABLE customers (
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+	Name VARCHAR(255) NOT NULL,
+    Status VARCHAR(255),
+    Total_Flight_Mileage INT
+
 );
 
 CREATE TABLE airline (
-	Customer_Name VARCHAR(255),
-	Customer_Status VARCHAR(255),
+	Customer_Id INT,
 	Flight_Number VARCHAR(255),
 	Aircraft VARCHAR(255),
 	Total_Aircraft_Seats INT,
 	Flight_Mileage INT,
-	Total_Customer_Mileage INT
+
+	FOREIGN KEY (Customer_Id) REFERENCES customers(Id)
+
 );
 
-CREATE TABLE customers (
-    Name VARCHAR(255),
-    Status VARCHAR(255),
-    Number_of_flight VARCHAR(255)
-);
 
-INSERT INTO blog (author,title,word_count,views) VALUES
-('Maria Charlotte','Best Paint Colors',814,14),
-('Juan Perez','Small Space Decorating Tips',1146,221),
-('Maria Charlotte','Hot Accessories',986,105),
-('Maria Charlotte','Mixing Textures',765,22),
-('Juan Perez','Kitchen Refresh',1242,307),
-('Maria Charlotte','Homemade Art Hacks',1002,193),
-('Gemma Alcocer','Refinishing Wood Floors',1571,7542);
+INSERT INTO customers (Name, Status,  Total_Flight_Mileage)
+VALUES
+    ('Agustine Riviera', 'Silver',115235),
+    ('Alaina Sepulvida', 'None',6008),
+    ('Tom Jones', 'Gold',205767),
+    ('Sam Rio', 'None',2653),
+    ('Jessica James', 'Silver',127656),
+    ('Ana Janco', 'Silver',136773),
+    ('Jennifer Cortez', 'Gold',300582),
+    ('Christian Janco', 'Silver',14642);
 
-INSERT INTO airline (Customer_Name, Customer_Status, Flight_Number, Aircraft, Total_Aircraft_Seats, Flight_Mileage, Total_Customer_Mileage) VALUES
-('Agustine Riviera','Silver','DL143','Boeing 747',400,135,115235),
-('Agustine Riviera','Silver','DL122','Airbus A330',236,4370,115235),
-('Alaina Sepulvida','None','DL122','Airbus A330',236,4370,6008),
-('Tom Jones','Gold','DL122','Airbus A330',236,4370,205767),
-('Tom Jones','Gold','DL53','Boeing 777',264,2078,205767),
-('Sam Rio','None','DL143','Boeing 747',400,135,2653),
-('Tom Jones','Gold','DL222','Boeing 777',264,1765,205767),
-('Jessica James','Silver','DL143','Boeing 747',400,135,127656),
-('Sam Rio','None','DL143','Boeing 747',400,135,2653),
-('Ana Janco','Silver','DL222','Boeing 777',264,1765,136773),
-('Jennifer Cortez','Gold','DL222','Boeing 777',264,1765,300582),
-('Jessica James','Silver','DL122','Airbus A330',236,4370,127656),
-('Sam Rio','None','DL37','Boeing 747',400,531,2653),
-('Christian Janco','Silver','DL222','Boeing 777',264,1765,14642);
+INSERT INTO airline (Customer_Id, Flight_Number, Aircraft, Total_Aircraft_Seats,Flight_Mileage) VALUES
+    (1, 'DL143','Boeing 747', 400, 135),
+    (1, 'DL122','Airbus A330', 236, 4370),
+    (2, 'DL122','Airbus A330', 236, 4370),
+    (3, 'DL122','Airbus A330', 236, 4370),
+    (3, 'DL53','Boeing 777', 264, 2078),
+    (4, 'DL143','Boeing 747', 400, 135),
+    (3, 'DL222','Boeing 777', 264, 1765),
+    (5, 'DL143','Boeing 747', 400, 135),
+    (4, 'DL37','Boeing 747', 400, 531),
+    (6, 'DL222','Boeing 777', 264, 1765),
+    (7, 'DL222','Boeing 777', 264, 1765),
+    (5, 'DL122','Airbus A330', 236, 4370),
+    (4, 'DL143','Boeing 747', 400, 135),
+    (8, 'DL222','Boeing 777', 264, 1765);
 
-INSERT INTO customers (Name,Status,Number_of_flight) VALUES
-('Agustine Riviera','Silver','DL143'),
-('Agustine Riviera','Silver','DL122'),
-('Alaina Sepulvida','None','DL122'),
-('Tom Jones','Gold','DL122'),
-('Tom Jones','Gold','DL53'),
-('Sam Rio','None','DL143'),
-('Tom Jones','Gold','DL222'),
-('Jessica James','Silver','DL143'),
-('Sam Rio','None','DL143'),
-('Ana Janco','Silver','DL222'),
-('Jennifer Cortez','Gold','DL222'),
-('Jessica James','Silver','DL122'),
-('Sam Rio','None','DL37'),
-('Christian Janco','Silver','DL222');
 
 -- Task 1
 SELECT COUNT(Flight_Number) AS total_flight_numbers FROM airline;
@@ -78,14 +89,14 @@ SELECT AVG(Flight_Mileage) AS average_flight_distance FROM airline;
 SELECT AVG(Total_Aircraft_Seats) AS average_number_of_seats FROM airline;
 
 -- Task 4
-SELECT Customer_Status, AVG(Total_Customer_Mileage) AS average_miles_flown
-FROM airline
-GROUP BY Customer_Status;
+SELECT Status, AVG(Total_Flight_Mileage) AS average_miles_flown
+FROM customers
+GROUP BY Status;
 
 -- Task 5
-SELECT Customer_Status, MAX(Total_Customer_Mileage) AS max_number_of_miles_flown_by_status
-FROM airline
-GROUP BY Customer_Status;
+SELECT Status, MAX(Total_Flight_Mileage) AS max_number_of_miles_flown_by_status
+FROM customers
+GROUP BY Status;
 
 -- Task 6
 SELECT COUNT(*) AS total_boeing_aircraft
@@ -100,15 +111,15 @@ where Flight_Mileage between 300 and 2000;
 -- Task 8
 SELECT c.Status, AVG(a.Flight_Mileage) AS average_flight_distance_booked_by_status
 FROM customers c
-JOIN airline a ON c.Number_of_flight = a.Flight_Number
+JOIN airline a ON c.Id = a.Customer_Id
 GROUP BY c.Status;
 
 -- Task 9
 SELECT c.Status, a.Aircraft, COUNT(*) AS most_often_booked_aircraft_by_gold_status
 FROM customers c
-JOIN airline a ON c.Number_of_flight = a.Flight_Number
+JOIN airline a ON c.Id = a.Customer_Id
 WHERE c.Status = 'Gold'
 GROUP BY c.Status, a.Aircraft
-ORDER BY bookings_count DESC
+ORDER BY most_often_booked_aircraft_by_gold_status DESC
 LIMIT 1;
 
